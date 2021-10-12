1c0dbc0424570b6ace5de80d95e9dc951652d4f6
MooD25 <johndoe@example.commarcoleedillon@hotmail.com>
2021-10-12 00:27:18 -0400

    commit

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ require('dotenv').config({ path: 'config/keys.env' });
 const customerController = require("./controllers/customerController.js");
 const productController = require("./controllers/productController.js");
 
+
+const PORT = 300;
 if (process.env.NODE_ENV != "production") {
     require('dotenv').config({ path: 'config/keys.env' });
 }
