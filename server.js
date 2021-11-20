387e7a1b920160318fc1fc5483f9fc984e63017f
MooD25 <johndoe@example.commarcoleedillon@hotmail.com>
2021-11-20 01:30:07 -0500

    commit on master

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV != "production") {
 const app = express();
 
 const corsOptionsDelegate = function (req, callback) {
-    const allowlist = [`http://localhost:3000`, 'http://127.0.0.1:3001', 'https://hardcore-yonath-f927b7.netlify.app']
+    const allowlist = [`http://localhost:3000`, 'http://127.0.0.1:3001', 'https://61989568c6e9b4b439bbd4ba--hardcore-yonath-f927b7.netlify.app/']
     let corsOptions;
     if (allowlist.indexOf(req.header('Origin')) !== -1) {
         corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
